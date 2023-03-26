e4319b2531507ac042f242d7d958a6dcd146b64d
vincentvarona <vincentvarona1@gmail.com>
2023-03-26 16:23:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const menuDesp = document.getElementById("menudesp");
+const hamb = document.getElementById("hamb");
+
+hamb.addEventListener("click", () => {
+	menuDesp.style.display = "block";
+	hamb.style.display = "none";
+ })
+
+
+
